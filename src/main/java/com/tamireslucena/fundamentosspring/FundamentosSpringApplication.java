1592ac10521f0f594532be8ac68b03d248181fa4
tamireslucena <tamiresb.lucena@gmail.com>
2023-07-05 19:25:52 -0300

    config: first commit

---
+++ src/main/java/com/tamireslucena/fundamentosspring/FundamentosSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tamireslucena.fundamentosspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosSpringApplication.class, args);
+	}
+
+}
